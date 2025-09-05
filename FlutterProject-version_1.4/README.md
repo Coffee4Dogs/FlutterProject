9e7da49877d0def035c436a68f1efcd7969ba78c
Jack H. Lagos <59121551+Coffee4Dogs@users.noreply.github.com>
2025-09-05 17:04:02 -0600

    Add files via upload

---
+++ FlutterProject-version_1.4/README.md
@@ -0,0 +1,4 @@
+# Flutter Project
+Delta Labs Group
+
+![Texto alternativo](https://raw.githubusercontent.com/Coffee4Dogs/FlutterProject/refs/heads/Concept/assets/concept.png)
---
+++ FlutterProject-version_1.4/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'screens/Get Started/GetStarted_Background.dart';
+import 'screens/Get Started/GetStarted_Button.dart';
+import 'screens/Get Started/GetStarted_LegalButton.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+
+    // Recordatorio: Solo hay un MaterialApp y un Scaffold. 
+    return MaterialApp(
+      home: Scaffold(
+        body: 
+          Stack(
+          // Llamar los widgets y superponerlos (no usando row o column):
+          children: [
+            GetStarted_Background(),
+            GetStarted_BottomLeftButton(),
+            LegalButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
